dc73ec3f4e9505ea37c9fc3ffd2ca2f5c2bcfc4d
KaanSerin <kaanserin113@gmail.com>
2023-06-02 10:09:39 +0300

    Implement register method on auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -27,8 +27,54 @@ export class AuthService {
     return { access_token: await this.jwtService.sign(payload) };
   }
 
-  // @TODO: Implement
   async register(data: CreateUserValidator) {
-    return data;
+    const emailInUse = await this.prisma.user.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (emailInUse) {
+      throw new HttpException('Email already in use', HttpStatus.BAD_REQUEST);
+    }
+
+    const usernameInUse = await this.prisma.user.findFirst({
+      where: {
+        username: data.username,
+      },
+    });
+
+    if (usernameInUse) {
+      throw new HttpException(
+        'Username already in use',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const role = await this.prisma.userRole.findFirst({
+      where: {
+        id: data.roleId,
+      },
+    });
+
+    if (!role) {
+      throw new HttpException('Invalid role', HttpStatus.BAD_REQUEST);
+    }
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: data.email,
+          username: data.username,
+          password: await bcrypt.hash(data.password, 10),
+          roleId: role.id,
+        },
+      });
+
+      const payload = { id: user.id, username: user.username };
+      return { access_token: await this.jwtService.signAsync(payload) };
+    } catch (e) {
+      throw new HttpException(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
