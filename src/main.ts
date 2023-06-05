5c4579ee36fdd914bffe9d3d59d3a8264da74b1f
KaanSerin <kaanserin113@gmail.com>
2023-06-05 21:27:10 +0300

    Update cors options

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN,
+  });
   app.use(cookieParser());
   await app.listen(process.env.PORT);
 }
