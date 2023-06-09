2ee1d5278294f8923be0be0bb232c57899eeb5c3
KaanSerin <kaanserin113@gmail.com>
2023-06-09 16:21:19 +0300

    Add additional fields to users table

---
+++ prisma/migrations/20230609130435_add_first_name_last_name_bio_gender_profile_pic_url_columns_to_users_table/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "bio" TEXT,
+ADD COLUMN     "first_name" VARCHAR(50),
+ADD COLUMN     "gender" TEXT,
+ADD COLUMN     "last_name" VARCHAR(50),
+ADD COLUMN     "profile_pic_url" TEXT;
